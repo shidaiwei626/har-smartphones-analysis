424968c00a01d7e83fce1fa98c60dc2851c78a6f
Wagner Pinheiro <wagner.pinheiro@mandic.net.br>
2017-02-02 17:25:33 -0200

    added feature_labels

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,10 @@
 
 # library()
 
+# Set the default configuration settings
 HAR.Config.url <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
 HAR.Config.data_path = "./data/"
 HAR.Config.file_zipped <- 'dataset.zip'
-
-setwd(".")
 
 #' Download e unzip data files
 #' @param config Configuration settings
@@ -28,16 +27,31 @@ HAR.download <-function(Config){
 #' @param dataset train|test
 #'
 HAR.load <- function(Config, dataset = "train"){
-  x <- read.table(paste0(Config.data_path, 'dataset/', dataset , '/X_' , dataset , '.txt'))
+  feature_list <- read.table(paste0(Config.data_path, 'dataset/features.txt'), col.names = c("id", "name"))
+  activity_labels <- read.table(paste0(Config.data_path, 'dataset/activity_labels.txt'), col.names = c("id", "activity"))
+  
+  # clean features names
+  feature_labels <-feature_list[,"name"]
+  feature_labels <- gsub('\\(\\)', '',  feature_labels)
+  feature_labels <- gsub('\\)', '',  feature_labels)
+  feature_labels <- gsub(',', '-',  feature_labels)
+  feature_labels <- gsub('\\(', '-',  feature_labels)
+  
+  x <- read.table(paste0(Config.data_path, 'dataset/', dataset , '/X_' , dataset , '.txt'), col.names=feature_labels)
   y <- read.table(paste0(Config.data_path, 'dataset/', dataset , '/y_' , dataset , '.txt'), col.names=c("activity"))
+  
+  # 3. Uses descriptive activity names to name the activities in the data set
+  
+  
   sub <- read.table(paste0(Config.data_path, 'dataset/', dataset , '/subject_' , dataset , '.txt'), col.names=c("subject"))
-  data <- cbind(sub, x, y)
+  
+  data <- cbind(x, sub, y)
   data
 }
 
 #' 1. Merges the training and the test sets to create one data set.
+#' @param Config configuration settings
 #' 
-
 HAR.loadAndMerge <- function(Config){
   train_data <- HAR.load(Config, 'train')
   test_data <- HAR.load(Config, 'test')
@@ -47,9 +61,13 @@ HAR.loadAndMerge <- function(Config){
 
 
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+# to-do
 
 # 3. Uses descriptive activity names to name the activities in the data set
+# Maked for whole dataset in the HAR.load function, see line 42
 
 # 4. Appropriately labels the data set with descriptive variable names.
+# to-do
 
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# to-do
